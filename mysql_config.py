56aaf5a7ba3971e91240d2a585a4fe921a4f86c8
manan-parekh-13 <manan.parekh@ofbusiness.in>
2024-05-16 09:40:43 +0530

    mysql port env variable added

--- mysql_config.py
+++ mysql_config.py
@@ -6,7 +6,7 @@ from kiteconnect.utils import get_sensitive_parameter
 mysql_user_name = get_sensitive_parameter("MYSQL_USER_NAME")
 mysql_password = get_sensitive_parameter("MYSQL_PASSWORD")
 mysql_host = get_sensitive_parameter("MYSQL_HOST")
-mysql_port = 3307  # Update with the new port number
+mysql_port = get_sensitive_parameter("MYSQL_PORT")
 
 # Database connection URL
 DATABASE_URL = f"mysql+mysqlconnector://{mysql_user_name}:{mysql_password}@{mysql_host}:{mysql_port}/pokerPhase"
