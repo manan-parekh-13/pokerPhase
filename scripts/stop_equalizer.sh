e61e80e83f85ef6aa880cd9f33dce01b2e38a98f
manan-parekh-13 <manan.parekh@ofbusiness.in>
2024-05-23 02:10:42 +0530

    added slack message for stop script

--- scripts/stop_equalizer.sh
+++ scripts/stop_equalizer.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+
+# Configuration
+SLACK_WEBHOOK_URL="https://hooks.slack.com/services/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"
+
+# Functions
+send_slack_message() {
+  local message=$1
+  curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${message}\"}" $SLACK_WEBHOOK_URL
+}
+
 pkill -f "flask"
+
+send_slack_message "Stopped the flask server";
