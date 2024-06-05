477f21c686a8e44eb7c8d5d054b28d0abef62500
manan-parekh-13 <manan.parekh@ofbusiness.in>
2024-06-05 09:06:27 +0530

    time diff check removed from ticker validation

--- equalizer/service/ticker_service.py
+++ equalizer/service/ticker_service.py
@@ -19,8 +19,8 @@ def is_ticker_valid(ticker):
     time_difference = set_timezone_in_datetime(datetime.now()) - set_timezone_in_datetime(ticker['exchange_timestamp'])
     allowed_time_difference = timedelta(seconds=2)
     # todo @manan - condition can be removed once we get data for every instrument_token
-    if time_difference > allowed_time_difference:
-        return False
+    # if time_difference > allowed_time_difference:
+    #     return False
 
     mode = ticker['mode']
     if mode == 'full':
