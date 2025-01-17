33376aece65c115ba7158153155039d0cf79ab48
manan-parekh-13 <manan.parekh@ofbusiness.in>
2025-01-17 13:55:05 +0530

    corrected env path in loader

--- environment/loader.py
+++ environment/loader.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 
 def load_environment(env):
-    env_file = os.path.join('environment', f"{env}.env")
-    load_dotenv(env_file)
+    env_file = os.path.join('../environment', f"{env}.env")
+    load_dotenv(dotenv_path=env_file, override=True)
