ef9922d74ef60bac4d2157c522072a61f1e929e0
manan-parekh-13 <manan.parekh@ofbusiness.in>
2025-01-21 14:26:01 +0530

    replaced product type with int

--- equalizer/service/ticker_service.py
+++ equalizer/service/ticker_service.py
@@ -48,7 +48,7 @@ def check_tickers_for_arbitrage(ticks, tickers_to_be_saved, web_socket, kite_cli
 
         opportunity = check_arbitrage(latest_tick_for_equivalent, latest_tick_for_instrument,
                                       instrument.threshold_spread_coef, instrument.min_profit_percent,
-                                      instrument.product_type, max_buy_quantity, web_socket.ws_id)
+                                      instrument.product_type_int, max_buy_quantity, web_socket.ws_id)
 
         if not opportunity:
             continue
