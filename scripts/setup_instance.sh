adf8965f9c7e16bfc0c6dbab1c61792c851a2451
manan-parekh-13 <manan.parekh@ofbusiness.in>
2025-02-15 19:46:24 +0530

    added dummy setup script

---
+++ scripts/setup_instance.sh
@@ -0,0 +1,9 @@
+SLACK_WEBHOOK_URL="https://hooks.slack.com/services/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"
+
+# Functions
+send_slack_message() {
+  local message=$1
+  curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${message}\"}" $SLACK_WEBHOOK_URL
+}
+
+send_slack_message "Now are we talking"
