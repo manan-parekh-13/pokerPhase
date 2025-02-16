aece049287248b799e4c829f4d6af01565b78254
manan-parekh-13 <manan.parekh@ofbusiness.in>
2025-02-16 12:22:50 +0530

    corrected env name

--- scripts/setup_instance.sh
+++ scripts/setup_instance.sh
@@ -75,7 +75,7 @@ echo "Environment variables added."
 # ---------------- SETUP MYSQL ----------------
 sudo docker pull mysql:8
 echo "MySQL image pulled successfully."
-sudo docker run --name mysql-server -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -d -p 3308:3306 mysql:8
+sudo docker run --name mysql-server -e MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD -d -p 3308:3306 mysql:8
 echo "MySQL container started."
 
 # ---------------- SETUP PokerPhase ENVIRONMENT ----------------
