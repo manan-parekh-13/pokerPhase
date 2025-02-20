281961f24b8a6eb6dd030011314add8eb005353e
manan-parekh-13 <manan.parekh@ofbusiness.in>
2025-02-21 01:50:32 +0530

    corrected startup script error

--- scripts/start_equalizer.sh
+++ scripts/start_equalizer.sh
@@ -18,7 +18,7 @@ send_slack_message() {
 cd "$APP_DIR"
 
 # Activate virtual environment
-sudo source "/pokerPhase/bin/activate" || { send_slack_message "Failed to activate virtual environment"; exit 1; }
+sudo source "/pokerPhase/myenv/bin/activate" || { send_slack_message "Failed to activate virtual environment"; exit 1; }
 
 # Pull latest code from Git
 git pull origin master >> "$LOG_FILE" 2>&1 || { send_slack_message "Git pull failed"; exit 1; }
