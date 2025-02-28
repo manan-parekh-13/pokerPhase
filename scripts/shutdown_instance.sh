d5de2a6bf3d84f721ee53c2765656e274483cbfe
manan-parekh-13 <manan.parekh@ofbusiness.in>
2025-02-28 15:34:01 +0530

    corrrected mysql dump cmds in shutdown script

--- scripts/shutdown_instance.sh
+++ scripts/shutdown_instance.sh
@@ -1,6 +1,5 @@
 # create backup and send it
-docker exec -it mysql-server /bin/bash
-mysqldump -u root -p'canyouseemypassword' --verbose pokerPhase > /backup/db_backup.sql
-exit
-sudo pigz -k /backup/db_backup.sql
-aws s3 cp /backup/db_backup.sql.gz s3://poker-phase-mysql/db_backup.sql.gz --debug
+current_datetime=$(date +"%Y-%m-%d_%H:%M:%S")
+sudo docker exec -i mysql-server mysqldump -u root -p"$MYSQL_PASSWORD" --verbose --no-create-info pokerPhase arbitrage_opportunities order_info raw_ticker_data | sudo tee "/backup/db_backup_${current_datetime}.sql" > /dev/null
+sudo pigz -k "/backup/db_backup_${current_datetime}.sql"
+aws s3 cp "/backup/db_backup_${current_datetime}.sql.gz" "s3://poker-phase-mysql/db_backup_${current_datetime}.sql.gz" --debug
